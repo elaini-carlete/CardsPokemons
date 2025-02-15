33172b73b0134660e3b37792586083506fcc9df1
Elaini Carlete Thomazelli <elainicarletethomazelli@gmail.com>
2025-02-14 21:42:19 -0300

    adicionado a organização inicial de pastas e arquivos e realizada a chamada da API Pokémon

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+//CardPokemon é a página inicial
+import CardPokemon from './components/CardPokemon.jsx'
+import '../src/global.css'
+
+function App() {
+
+  return (
+    <>
+      <CardPokemon />
+    </>
+  )
+
+}
+
+export default App
---
+++ src/api/fetchPokemon.jsx
@@ -0,0 +1,21 @@
+//Chamada da API para buscar os pokemons
+export const fetchPokemon = async (offset) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?limit=10&offset=${offset}`;
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const detailDataPromises = data.results.map(async (pokemon) => {
+        const detailPokemon = await fetch(pokemon.url);
+        const responseData = await detailPokemon.json();
+
+        return{
+            name: responseData.name,
+            image: responseData.sprites.front_default,
+            type: responseData.types[0].type.name
+        }
+    });
+
+    const detailData = await Promise.all(detailDataPromises);
+
+    return detailData;
+}
